1cc810fa9fadefc551ad651479b06fe411108b87
Abu Zafor <idxbdff@gmail.com>
2025-09-28 10:38:52 +0600

    Update app.py

--- app.py
+++ app.py
@@ -73,7 +73,7 @@ def like():
     return jsonify({
         
         
-        "credits":"great.thug4ff.com",
+        "credits":"TOXIC XITERS",
         "likes_added": after_like - before_like,
         "likes_before": before_like,
         "likes_after": after_like,
